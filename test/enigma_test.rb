34d4a6f3a486c029d5fc79389deeeb2bac5b76ce
Eugene Theriault <hybridbassist@gmail.com>
2020-09-17 20:03:16 -0700

    Create test for method to get today's date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 
 require './lib/enigma'
 
@@ -10,5 +11,10 @@ class EnigmaTest < MiniTest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_track_date
+    Date.stubs(:today).returns(Date.new(2020,9,17))
+    assert_equal "091720", @enigma.date
   end
 end
