dc7f10ee4b8ec09614a3b83411674f3fbd737f72
Eugene Theriault <hybridbassist@gmail.com>
2020-09-17 20:26:02 -0700

    Create test for method to generate keys

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,4 +17,9 @@ class EnigmaTest < MiniTest::Test
     Date.stubs(:today).returns(Date.new(2020,9,17))
     assert_equal "091720", @enigma.todays_date
   end
+
+  def test_it_can_generate_a_key
+    Array.stubs(:sample).returns([2, 4, 6, 8, 0])
+    assert_equal "24680", @enigma.generate_key
+  end
 end
