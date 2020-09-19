264be243a44178698ccb70f3450fde314b66e248
Eugene Theriault <hybridbassist@gmail.com>
2020-09-18 21:50:10 -0700

    Create decrypt runner functionality and associated text file

---
+++ lib/decrypt.rb
@@ -0,0 +1,16 @@
+require './lib/enigma'
+
+ARGV
+
+encrypted_message = File.open(ARGV[0], "r")
+encrypted_text = encrypted_message.read
+encrypted_message.close
+
+enigma = Enigma.new
+require "pry";binding.pry 
+decoded = enigma.decrypt(encrypted_text, ARGV[2], ARGV[3])
+decrypted_file = File.open(ARGV[1], 'w')
+decrypted_file.write(decoded[:decryption])
+decrypted_file.close
+
+puts "Created '#{ARGV[1]}' with the key #{decoded[:key]} and date #{decoded[:date]}"
