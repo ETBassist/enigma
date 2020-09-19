4de399f73275b526e7916f5489bb39314c150e21
Eugene Theriault <hybridbassist@gmail.com>
2020-09-18 19:35:14 -0700

    Create test for decrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -46,4 +46,13 @@ class EnigmaTest < MiniTest::Test
     }
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
+
+  def test_it_can_decrypt_text_with_given_parameters
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
