7c485e10698c57f5d7252b83e798b397bf635b8a
Eugene Theriault <hybridbassist@gmail.com>
2020-09-18 18:45:25 -0700

    Add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -23,4 +23,27 @@ class Enigma
   def generate_shifts(key, offsets)
     key.zip(offsets).map(&:sum)
   end
+
+  def encrypt(message, digits = key_digits, date = todays_date)
+    alphabet = ("a".."z").to_a << " "
+    key = make_key(digits)
+    offset = make_offsets(date)
+    shifts = generate_shifts(key, offset)
+    counter = 0
+    # figure out how to refactor using with_index later
+    encoded = message.split('').map do |character|
+      counter += 1
+      if counter == 1
+        alphabet.rotate(shifts[0])[alphabet.index(character)]
+      elsif counter == 2
+        alphabet.rotate(shifts[1])[alphabet.index(character)]
+      elsif counter == 3
+        alphabet.rotate(shifts[2])[alphabet.index(character)]
+      else
+        counter = 0
+        alphabet.rotate(shifts[3])[alphabet.index(character)]
+      end
+    end.join
+    { encryption: encoded, key: digits, date: date }
+  end
 end
