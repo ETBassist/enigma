ad88319f36ace0095a06892efc1020dfde6e5dfe
Eugene Theriault <hybridbassist@gmail.com>
2020-09-19 12:53:43 -0700

    Change key_digits method to use rand instead of sample and begin crack method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,7 +6,11 @@ class Enigma
   end
 
   def key_digits
-    (0..9).to_a.sample(5).join
+    digits = []
+    5.times do 
+      digits << rand(9)
+    end
+    digits.join
   end
 
   def make_key(digits)
@@ -26,9 +30,9 @@ class Enigma
   def encrypt(message, digits = key_digits, date = todays_date)
     alphabet = ('a'..'z').to_a << ' '
     shifts = generate_shifts(make_key(digits), make_offsets(date))
-    encoded = message.split('').map.with_index do |character, character_index|
+    encoded = message.downcase.split('').map.with_index do |character, char_index|
       if alphabet.include?(character)
-        alphabet.rotate(shifts[character_index % 4])[alphabet.index(character)]
+        alphabet.rotate(shifts[char_index % 4])[alphabet.index(character)]
       else
         character
       end
@@ -39,13 +43,29 @@ class Enigma
   def decrypt(message, digits, date = todays_date)
     alphabet = ('a'..'z').to_a << ' '
     shifts = generate_shifts(make_key(digits), make_offsets(date))
-    decoded = message.split('').map.with_index do |character, character_index|
+    decoded = message.split('').map.with_index do |character, char_index|
       if alphabet.include?(character)
-        alphabet.rotate(-shifts[character_index % 4])[alphabet.index(character)]
+        alphabet.rotate(-shifts[char_index % 4])[alphabet.index(character)]
       else
         character
       end
     end.join
     { decryption: decoded, key: digits, date: date }
   end
+
+  def crack(message, date = todays_date)
+    alphabet = ('a'..'z').to_a << ' '
+    known_characters = ['d', 'n', 'e', ' ']
+    make_offsets(date)
+  end
+# possible generation of key from cracked digits
+# keys = [2, 27, 71, 15]
+# digits = keys.flat_map.with_index do |key, key_index|
+#   if key_index != 0
+#      until key.digits.last == keys[key_index - 1].digits.first
+#         key += shifts[key_index]
+#      end
+#      key.digits.reverse
+# end.uniq
+#=> [2, 7, 1, 5]
 end
