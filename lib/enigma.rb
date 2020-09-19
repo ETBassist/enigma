7d50e9feb006bf5c110b406948f040ccd74cda9b
Eugene Theriault <hybridbassist@gmail.com>
2020-09-18 19:08:53 -0700

    Refactor encrypt to use with_index

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -29,20 +29,8 @@ class Enigma
     key = make_key(digits)
     offset = make_offsets(date)
     shifts = generate_shifts(key, offset)
-    counter = 0
-    # figure out how to refactor using with_index later
-    encoded = message.split('').map do |character|
-      counter += 1
-      if counter == 1
-        alphabet.rotate(shifts[0])[alphabet.index(character)]
-      elsif counter == 2
-        alphabet.rotate(shifts[1])[alphabet.index(character)]
-      elsif counter == 3
-        alphabet.rotate(shifts[2])[alphabet.index(character)]
-      else
-        counter = 0
-        alphabet.rotate(shifts[3])[alphabet.index(character)]
-      end
+    encoded = message.split('').map.with_index do |character, character_index|
+      alphabet.rotate(shifts[character_index % 4])[alphabet.index(character)]
     end.join
     { encryption: encoded, key: digits, date: date }
   end
