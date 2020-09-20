6373933e83b24f0546e457a120ae74060cd2f58f
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 15:58:52 -0700

    Change encrypt and decrypt to ignore slashes in date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,7 +13,7 @@ class Enigma < Rotor
     offsets = @key_gen.make_offsets(date.delete('/'))
     shifts = @key_gen.generate_shifts(key, offsets)
     encoded = shift_letters(message, shifts)
-    { encryption: encoded, key: digits, date: date }
+    { encryption: encoded, key: digits, date: date.delete('/') }
   end
 
   def decrypt(message, digits, date = @key_gen.todays_date)
@@ -21,7 +21,7 @@ class Enigma < Rotor
     offsets = @key_gen.make_offsets(date.delete('/'))
     shifts = @key_gen.generate_shifts(key, offsets)
     decoded = shift_letters(message, shifts, 'backwards')
-    { decryption: decoded, key: digits, date: date }
+    { decryption: decoded, key: digits, date: date.delete('/') }
   end
 
   def crack(message, date = @key_gen.todays_date)
