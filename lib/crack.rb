67d24a5abf11dc06c6fb922070b262b1a054110f
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 14:07:49 -0700

    Add word to crack runner output

--- lib/crack.rb
+++ lib/crack.rb
@@ -4,6 +4,7 @@ arguments = ARGV
 encrypted_filename = arguments[0]
 file_to_write = arguments[1]
 date = arguments[2]
+
 encrypted_message = File.open(encrypted_filename, "r")
 encrypted_text = encrypted_message.read
 encrypted_message.close
@@ -15,4 +16,4 @@ decrypted_file = File.open(file_to_write, 'w')
 decrypted_file.write(decoded[:decryption])
 decrypted_file.close
 
-puts "Created '#{file_to_write}' with the key #{decoded[:key]} and date #{decoded[:date]}"
+puts "Created '#{file_to_write}' with the cracked key #{decoded[:key]} and date #{decoded[:date]}"
