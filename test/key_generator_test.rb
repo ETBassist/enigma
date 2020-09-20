9f858c8c1e2d1254832ae1e0c01b9332f1162541
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 10:57:06 -0700

    Create test for todays_date method in Key Generator

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 
 require './lib/key_generator'
+require 'mocha/minitest'
 
 class KeyGeneratorTest < MiniTest::Test
   def setup
@@ -9,5 +10,10 @@ class KeyGeneratorTest < MiniTest::Test
 
   def test_it_exists
     assert_instance_of KeyGenerator, @key_generator
+  end
+
+  def test_it_track_date
+    Date.stubs(:today).returns(Date.new(2020, 9, 17))
+    assert_equal '091720', @key_generator.todays_date
   end
 end
