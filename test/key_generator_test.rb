5622f0b71f6d927dc4aae49b01eb79c27e6dfa64
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 11:00:45 -0700

    Create test for generating key digits

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -12,8 +12,15 @@ class KeyGeneratorTest < MiniTest::Test
     assert_instance_of KeyGenerator, @key_generator
   end
 
-  def test_it_track_date
+  def test_it_tracks_date
     Date.stubs(:today).returns(Date.new(2020, 9, 17))
     assert_equal '091720', @key_generator.todays_date
+  end
+
+  def test_it_can_make_key_digits
+    numbers = (0..9).to_a
+    @enigma.key_digits.split('').each do |number|
+      assert_includes numbers, number.to_i
+    end
   end
 end
