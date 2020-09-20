ec11e0ac846af256561802b171f4b50891aadae1
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 12:00:03 -0700

    Create test for shift letters method in Rotor

--- test/rotor_test.rb
+++ test/rotor_test.rb
@@ -10,4 +10,10 @@ class RotorTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Rotor, @rotor
   end
+
+  def test_it_can_shift_letters
+    shifts = [3, 27, 73, 20]
+    assert_equal 'keder ohulw!', @rotor.shift_letters('HELLO WORLD!', shifts)
+    assert_equal 'hello world!', @rotor.shift_letters('keder ohulw!', shifts, 'backwards')
+  end
 end
