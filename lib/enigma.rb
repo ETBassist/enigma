78c3e28d9b52b4ef7f2bcdf202db471200a6ae6e
Eugene Theriault <hybridbassist@gmail.com>
2020-09-20 15:15:00 -0700

    Change encrypt and decrypt to account for /'s in date input

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,21 +10,21 @@ class Enigma < Rotor
 
   def encrypt(message, digits = @key_gen.key_digits, date = @key_gen.todays_date)
     key = @key_gen.make_key(digits)
-    offsets = @key_gen.make_offsets(date)
+    offsets = @key_gen.make_offsets(date.delete('/'))
     shifts = @key_gen.generate_shifts(key, offsets)
     encoded = shift_letters(message, shifts)
     { encryption: encoded, key: digits, date: date }
   end
 
-  def decrypt(message, digits, date = todays_date)
+  def decrypt(message, digits, date = @key_gen.todays_date)
     key = @key_gen.make_key(digits)
-    offsets = @key_gen.make_offsets(date)
+    offsets = @key_gen.make_offsets(date.delete('/'))
     shifts = @key_gen.generate_shifts(key, offsets)
     decoded = shift_letters(message, shifts, 'backwards')
     { decryption: decoded, key: digits, date: date }
   end
 
-  def crack(message, date = todays_date)
+  def crack(message, date = @key_gen.todays_date)
     known_characters = ['d', 'n', 'e', ' ']
     actual_characters = message.split('')[-4..-1].reverse
     shifts = actual_characters.map.with_index do |character, char_index|
